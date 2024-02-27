a6b10df399b8cc6852161c7ebf76889008e67fe6
Natalia Rybalko <natasha.rybalko@gmail.com>
2024-02-27 22:01:15 +0200

    6_1 simple calculator

---
+++ app.py
@@ -0,0 +1,14 @@
+operation = input('''
+Please type what would you like to do:
++ for addition
+- for substraction
+''')
+
+number_1 = float(input('Enter your first number'))
+number_2 = float(input ('Enter your second number'))
+
+if operation == '+':
+    print(number_1 + number_2)
+
+elif operation == '-':
+    print(number_1 - number_2)
